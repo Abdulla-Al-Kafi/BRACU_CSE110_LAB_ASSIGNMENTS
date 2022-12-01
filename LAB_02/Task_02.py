c9f4e6e21d338fa2a19ad3c2496fc8bfb8ebb232
Abdulla Al Kafi <abdullaalkafi99@gmail.com>
2022-12-01 14:09:17 +0600

    This project is under development.

--- LAB_02/Task_02.py
+++ LAB_02/Task_02.py
@@ -1,7 +1,13 @@
-# while loop
-car_name = input("Enter your favorite car: ")
-num = int(input("Enter a number: "))
-count= 0
-while(count<num):
-    print(car_name)
-    count= count+1
+# # while loop
+# car_name = input("Enter your favorite car: ")
+# num = int(input("Enter a number: "))
+# count= 0
+# while(count<num):
+#     print(car_name)
+#     count= count+1
+#
+# # for loop
+# user_car= input("enter the fav car name: ")
+# num= int(input("enter the number: "))
+# for x in range(num):
+#     print(user_car)
