763088f164358979d9e5ae70b6ee8536c0c52141
Abdulla Al Kafi <abdullaalkafi99@gmail.com>
2022-12-01 13:49:30 +0600

    This project is under development.

---
+++ Task_01.py
@@ -0,0 +1,47 @@
+# AS there are multiple solutions I had to comment them out. Just uncomment them and you're ready to go
+
+# #a
+#
+# # initializing loop counter
+# counter = 24
+#
+# # loop structure
+# while counter >= -6:
+#
+#     # inside loop body
+#     if counter == -6:
+#         print(counter, end="")
+#     else:
+#         print(counter, end=", ")
+#
+#     counter = counter - 6  # updating loop counter
+
+# #b
+# counter= -10
+# while counter<=20:
+#     if counter==20:
+#         print(counter)
+#     else:
+#         print(counter,end=", ")
+#     counter= counter+5
+
+# #c
+# counter=-10
+# while counter<=20:
+#     if counter==20:
+#         print(counter)
+#     else:
+#         print(counter,end=', ')
+#     counter=counter+5
+
+# #d
+# counter=18
+# while counter<=63:
+#     if counter==63:
+#         print((-1)*counter)
+#     elif counter%2==0:
+#         print(counter,end=',')
+#     else:
+#         print((-1)*counter,end=",")
+#
+#     counter=counter+9
